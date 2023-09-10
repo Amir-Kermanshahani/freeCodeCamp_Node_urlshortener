a8d3939b6ae020ab73315983c3f7111e993e254b
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-09-10 13:11:57 +0330

    used async function

--- index.js
+++ index.js
@@ -48,31 +48,30 @@ app.get('/api/hello', function(req, res) {
 app.post('/api/shorturl', async function(req, res) {
   const url = req.body.url_input
   const urlCode = shortid.generate()
-  if (!dns.lookup(url)) {
-    res.status(401).json({
-      error: 'invalid URL'
-    })
-  } else {
-    try {
-      let findOne = await URL.findOne({
-        original_url: url
-      })
-      if (findOne) {
-        res.json({
-          original_url: url,
-          short_url: urlCode
+  dns.lookup(url, function(err, data) {
+    if (err) {res.status(401).json({error: 'invalid URL'})}
+    async () => {
+      try {
+        let findOne = await URL.findOne({
+          original_url: url
         })
-        await findOne.save()
-        res.json({
-          original_url: findOne.original_url,
-          short_url: findOne.short_url
-        })
+        if (findOne) {
+          res.json({
+            original_url: url,
+            short_url: urlCode
+          })
+          await findOne.save()
+          res.json({
+            original_url: findOne.original_url,
+            short_url: findOne.short_url
+          })
+        }
+      } catch (err) {
+        console.error(err)
+        res.status(500).json("Server error ...")
       }
-    } catch (err) {
-      console.error(err)
-      res.status(500).json("Server error ...")
     }
-  }
+  })
 })
 
 app.get('/api/shorturl/:short_url?', async function(req, res) {
